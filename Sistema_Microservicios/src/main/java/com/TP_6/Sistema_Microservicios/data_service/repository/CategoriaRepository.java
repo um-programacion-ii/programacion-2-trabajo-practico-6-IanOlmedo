1dc4038506f3725ba9479ed0b7a4972ac7841369
iolmedo <ianitoolmedo12@gmail.com>
2025-09-12 09:59:43 -0300

    Repository y Services

---
+++ Sistema_Microservicios/src/main/java/com/TP_6/Sistema_Microservicios/data_service/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.TP_6.Sistema_Microservicios.data_service.repository;
+
+import com.TP_6.Sistema_Microservicios.data_service.entity.Categoria;
+import com.TP_6.Sistema_Microservicios.data_service.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNombreIgnoreCase(String nombre);
+}
